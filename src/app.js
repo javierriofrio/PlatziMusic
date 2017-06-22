075581ac0d7644a7ea66615c194e7bae2091d7cf
Javier Riofrio <javierriofrio@gmail.com>
2017-06-22 09:53:18 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons'
+
+import ArtistList from './ArtistList'
+
+export default class PlatziMusic extends Component {
+  render() {
+    const artist = {
+      image: 'https://lastfm-img2.akamaized.net/i/u/300x300/31a51f6e3ec647c8997150ec837891c7.png',
+      name: 'David Bowie',
+      likes: 200,
+      comments: 140
+    }
+    const artists = Array(500).fill(artist)
+    return (
+      <View style={styles.container}>
+        <ArtistList artists={artists} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+    paddingTop: 50,
+  },
+});
+
+AppRegistry.registerComponent('PlatziMusic', () => PlatziMusic);
